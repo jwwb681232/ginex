b21b67d9c4e7101b6e6b784908dc2976e77ee3ab
wangxiao <wx497657341@qq.com>
2019-08-16 17:17:40 +0800

    validate

--- models/user.go
+++ models/user.go
@@ -4,49 +4,64 @@ import (
 	"time"
 	"ginex/database"
 	"fmt"
-	"database/sql"
+	"golang.org/x/crypto/bcrypt"
+	sql2 "database/sql"
 )
 
 type User struct {
-	Id            int
+	Id            int64
 	Name          string
 	Email         string
 	Password      string
-	RememberToken string
+	RememberToken *string
 	CreatedAt     time.Time
 	UpdatedAt     time.Time
-	DeletedAt     time.Time
-}
-
-var db *sql.DB
-
-func init() {
-	db = database.Init()
+	DeletedAt     *time.Time
 }
 
 func Get() {
 	user := User{}
+	/*db := database.Init()
 	row := db.QueryRow("SELECT * FROM users WHERE id = ?", 1)
 	err := row.Scan(&user.Id, &user.Name, &user.Email,&user.Password, &user.RememberToken, &user.CreatedAt, &user.UpdatedAt, &user.DeletedAt)
 	if err != nil {
 		fmt.Println(err)
-	}
+	}*/
 	fmt.Println(user)
 }
 
-func GetUser(email string) {
+func GetUser(email string) (User,error) {
 	user := User{}
-	row := db.QueryRow("SELECT * FROM users WHERE email = ?", email)
+	db := database.Init()
+	row := db.QueryRow("SELECT * FROM users WHERE email = ? LIMIT 1", email)
 	err := row.Scan(&user.Id, &user.Name, &user.Email,&user.Password, &user.RememberToken, &user.CreatedAt, &user.UpdatedAt, &user.DeletedAt)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(user)
+	return user,err
 }
 
 func StoreUser(user User) (int64, error) {
-	//todo store user data
-	//return db.Exec("INSERT INTO users(`name`,`email`,`password`) VALUES (?,?,?)",user.Name,user.Email,user.Password)
+	//初始化数据库
+	db := database.Init()
+
+	//获取这个邮箱是否存在
+	_,err := GetUser(user.Email)
+
+	//如果错误不是没找到数据（那就是找到了数据）就返回不继续执行
+	if err != sql2.ErrNoRows {
+		return 0,err
+	}
+
+	//插入数据
+	sql,_ := db.Prepare("INSERT INTO users(`name`,`email`,`password`,`created_at`,`updated_at`) VALUES (?,?,?,?,?)")
+	now := time.Now().Format("2006-01-02 15:04:05")
+	passwordHashed,_ := bcrypt.GenerateFromPassword([]byte(user.Password),bcrypt.DefaultCost)
+
+	result,_ := sql.Exec(user.Name,user.Email,passwordHashed,now,now)
+	id,err := result.LastInsertId()
+	if err != nil {
+		panic(err)
+	}
+
+	return id,err
 }
 
 /*func All() []User {
