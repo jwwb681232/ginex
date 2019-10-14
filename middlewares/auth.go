4106d03dea7f961c5e97da8349f085733ca65d27
wangxiao <wx497657341@qq.com>
2019-10-14 10:11:10 +0800

    fix fmt bug

--- middlewares/auth.go
+++ middlewares/auth.go
@@ -1,10 +1,10 @@
 package middlewares
 
 import (
+	"fmt"
 	"ginex/models"
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
-	"golang.org/x/tools/go/ssa/interp/testdata/src/fmt"
 	"net/http"
 )
 
@@ -14,7 +14,7 @@ func Auth() gin.HandlerFunc {
 		session := sessions.Default(c)
 		var value models.User
 		value = session.Get("ginex_session_key").(models.User)
-		fmt.Printf(value)
+		fmt.Println(value)
 		if value.ID != 0  {
 			c.Next()
 			return
